87d6623e5a99356b845b059c3ba3f44585cf9956
xadicha <mahkamovahadija1810@gmail.com>
2024-09-05 09:01:30 +0500

    sqlc generated

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xadichamahkamova/fitness-tracking-app
 
 go 1.22.6
+
+require github.com/sqlc-dev/pqtype v0.3.0
---
+++ schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE "users" (
+  "id" serial PRIMARY KEY,
+  "username" varchar,
+  "email" varchar,
+  "password_hash" varchar,
+  "profile" jsonb
+);
+
+CREATE TABLE "workouts" (
+  "id" serial PRIMARY KEY,
+  "user_id" int,
+  "date" varchar
+);
+
+CREATE TABLE "exercises" (
+  "id" serial PRIMARY KEY,
+  "workout_id" int,
+  "name" varchar
+);
+
+CREATE TABLE "sets" (
+  "id" serial PRIMARY KEY,
+  "exercise_id" int,
+  "repetitions" int,
+  "weight" float
+);
+
+CREATE TABLE "images" (
+  "id" serial PRIMARY KEY,
+  "user_id" int,
+  "url" varchar
+);
+
+ALTER TABLE "workouts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "exercises" ADD FOREIGN KEY ("workout_id") REFERENCES "workouts" ("id");
+
+ALTER TABLE "sets" ADD FOREIGN KEY ("exercise_id") REFERENCES "exercises" ("id");
+
+ALTER TABLE "images" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
