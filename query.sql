20c902210ee194ad401bdb92e1cedb8ec08c9e9e
xadicha <mahkamovahadija1810@gmail.com>
2024-09-05 09:22:16 +0500

    queries added

--- query.sql
+++ query.sql
@@ -1,3 +1,25 @@
 -- name: GetUser :one 
 SELECT * FROM users 
 WHERE id=$1 LIMIT 1;
+
+-- name: ListUsers :many 
+SELECT id, username, email, profile
+FROM users 
+ORDER BY username; 
+
+-- name: CreateUser :one 
+INSERT INTO users (username, password_hash, email, profile)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: UpdateUser :exec
+UPDATE users 
+    set 
+        username = $2,
+        email = $3,
+        profile = $4
+WHERE id = $1; 
+
+-- name: DeleteUser :exec 
+DELETE FROM users 
+WHERE id = $1;
--- storage/query.sql.go
+++ storage/query.sql.go
@@ -7,7 +7,51 @@ package storage
 
 import (
 	"context"
+	"database/sql"
+
+	"github.com/sqlc-dev/pqtype"
 )
+
+const createUser = `-- name: CreateUser :one
+INSERT INTO users (username, password_hash, email, profile)
+VALUES ($1, $2, $3, $4)
+RETURNING id, username, email, password_hash, profile
+`
+
+type CreateUserParams struct {
+	Username     sql.NullString
+	PasswordHash sql.NullString
+	Email        sql.NullString
+	Profile      pqtype.NullRawMessage
+}
+
+func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
+	row := q.db.QueryRowContext(ctx, createUser,
+		arg.Username,
+		arg.PasswordHash,
+		arg.Email,
+		arg.Profile,
+	)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Username,
+		&i.Email,
+		&i.PasswordHash,
+		&i.Profile,
+	)
+	return i, err
+}
+
+const deleteUser = `-- name: DeleteUser :exec
+DELETE FROM users 
+WHERE id = $1
+`
+
+func (q *Queries) DeleteUser(ctx context.Context, id int32) error {
+	_, err := q.db.ExecContext(ctx, deleteUser, id)
+	return err
+}
 
 const getUser = `-- name: GetUser :one
 SELECT id, username, email, password_hash, profile FROM users 
@@ -25,4 +69,71 @@ func (q *Queries) GetUser(ctx context.Context, id int32) (User, error) {
 		&i.Profile,
 	)
 	return i, err
+}
+
+const listUsers = `-- name: ListUsers :many
+SELECT id, username, email, profile
+FROM users 
+ORDER BY username
+`
+
+type ListUsersRow struct {
+	ID       int32
+	Username sql.NullString
+	Email    sql.NullString
+	Profile  pqtype.NullRawMessage
+}
+
+func (q *Queries) ListUsers(ctx context.Context) ([]ListUsersRow, error) {
+	rows, err := q.db.QueryContext(ctx, listUsers)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []ListUsersRow
+	for rows.Next() {
+		var i ListUsersRow
+		if err := rows.Scan(
+			&i.ID,
+			&i.Username,
+			&i.Email,
+			&i.Profile,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const updateUser = `-- name: UpdateUser :exec
+UPDATE users 
+    set 
+        username = $2,
+        email = $3,
+        profile = $4
+WHERE id = $1
+`
+
+type UpdateUserParams struct {
+	ID       int32
+	Username sql.NullString
+	Email    sql.NullString
+	Profile  pqtype.NullRawMessage
+}
+
+func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) error {
+	_, err := q.db.ExecContext(ctx, updateUser,
+		arg.ID,
+		arg.Username,
+		arg.Email,
+		arg.Profile,
+	)
+	return err
 }
